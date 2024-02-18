6561ffd9247a8f0b5be6010fb99c754ec8e0f122
Molnár Zoltán <mozo37@gmail.com>
2024-02-18 18:02:41 +0100

    game step5-6

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,5 +1,5 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour;
+
 var gamePattern = [];
 var userClickedPattern = [];
 
@@ -7,18 +7,26 @@ $(".btn").click( function () {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4);
-    randomChosenColour = buttonColours[randomNumber];
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("./sounds/" + name + ".mp3");
     audio.play();
 }
 
-$("button").on('click', function () {
-    nextSequence();
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
 }
-);
