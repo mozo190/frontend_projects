738e7aef6169749663ff8abedb48158afd543965
Molnár Zoltán <mozo37@gmail.com>
2024-02-18 16:27:07 +0100

    Commented out button click event and added mouseover event to change h1 color

--- jquery/index.js
+++ jquery/index.js
@@ -1,7 +1,11 @@
-$("button").click(function() {
-    $("h1").css("color", "red");
-});
+// $("button").click(function() {
+//     $("h1").css("color", "red");
+// });
 
 $("input").keypress(function(event) {
     $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "purple");
 });
