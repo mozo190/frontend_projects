09c1e0a315c7c8204831fd3dc81b6bbce3f6aa1d
Molnár Zoltán <mozo37@gmail.com>
2024-02-21 19:06:00 +0100

    Update code formatting and fix minor bugs

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,13 @@
+// var generateName = require('sillyname');
+
+// import generateName from 'sillyname';
+// var sillyName = generateName();
+
+// console.log('My name is ' + sillyName + '!');
+
+// const superheroes = require('superheroes');
+
+import superheroes from 'superheroes';
+var mySuperHeroName = superheroes.random();
+
+console.log('My superhero name is ' + mySuperHeroName + '!');
