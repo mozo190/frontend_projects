202726f193b47b9792b1d70a949ce10e11a082a8
Molnár Zoltán <mozo37@gmail.com>
2024-03-10 16:53:46 +0100

    Fix delete-secret API endpoint and update package name

--- BACKEND/5.5 REST APIs/index.js
+++ BACKEND/5.5 REST APIs/index.js
@@ -76,8 +76,8 @@ app.post("/delete-secret", async (req, res) => {
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
   try {
     const result = await axios.delete(
-      API_URL + "/secrets/",
-      searchId, config
+      API_URL + "/secrets/" + searchId,
+      config
       );
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
