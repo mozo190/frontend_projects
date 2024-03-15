584f20986a60e90434a9dc4067406903219ad493
Molnár Zoltán <mozo37@gmail.com>
2024-03-15 17:23:09 +0100

    Update port and database password in index.js and solution.js

--- BACKEND/8.5 Family Travel Tracker/index.js
+++ BACKEND/8.5 Family Travel Tracker/index.js
@@ -3,14 +3,14 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = 3030;
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
-  port: 5432,
+  password: "admin1234",
+  port: 3000,
 });
 db.connect();
 
