493d2cdc8e7da75771ab61d35b422794f79330f4
Molnár Zoltán <mozo37@gmail.com>
2024-03-15 15:23:17 +0100

    Add database connection and query to retrieve visited countries

--- BACKEND/8.3 Travel Tracker/index.js
+++ BACKEND/8.3 Travel Tracker/index.js
@@ -1,5 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "admin1234",
+  port: 3000,
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -9,6 +20,14 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  const result = await db.query("SELECTED country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach(country => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
