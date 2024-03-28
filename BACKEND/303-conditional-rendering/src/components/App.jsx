5c752cdf85b44853d57f33fe94df1c5d13912670
Molnár Zoltán <mozo37@gmail.com>
2024-03-28 18:20:33 +0100

    Fix bug in login functionality

---
+++ BACKEND/303-conditional-rendering/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return (
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="container">
+      {renderConditionally()}
+    </div>
+  );
+}
+
+export default App;
