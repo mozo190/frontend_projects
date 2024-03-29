8e222e935a6cf6034419a8193c5726283a980550
Molnár Zoltán <mozo37@gmail.com>
2024-03-29 09:38:41 +0100

    Add decrease button to App component

--- BACKEND/306-useState-hook/src/components/App.jsx
+++ BACKEND/306-useState-hook/src/components/App.jsx
@@ -10,10 +10,15 @@ function increase(){
   setCount(count + 1);  
 }
 
+function decrease(){
+  setCount(count - 1);
+}
+
 return(
   <div className="container">
     <h1>{count}</h1>
     <button onClick={increase}>+</button>
+    <button onClick={decrease}>-</button>
   </div>
 );
 }
