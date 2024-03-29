f72114b3decac9e65856776ee20f440927fc8a20
Molnár Zoltán <mozo37@gmail.com>
2024-03-29 08:55:48 +0100

    Fix bug in login functionality

---
+++ BACKEND/306-useState-hook/src/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var count =0;
+function increase(){
+  count++;
+  console.log(count);
+}
+
+ReactDOM.render(
+  <div className="container">
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
