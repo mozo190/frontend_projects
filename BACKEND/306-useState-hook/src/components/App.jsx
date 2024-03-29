496acd3ddf51fa9751df4a6d8157d317e3200585
Molnár Zoltán <mozo37@gmail.com>
2024-03-29 11:11:46 +0100

    Refactor App component to remove commented code

--- BACKEND/306-useState-hook/src/components/App.jsx
+++ BACKEND/306-useState-hook/src/components/App.jsx
@@ -22,39 +22,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState} from "react";
-
-
-// function App() {
-
-// const [count, setCount] = React.useState(0);
-
-
-// function increase(){
-//   setCount(count + 1);  
-// }
-
-// function decrease(){
-//   setCount(count - 1);
-// }
-
-// return(
-//   <div className="container">
-//     <h1>{count}</h1>
-//     <button onClick={increase}>+</button>
-//     <button onClick={decrease}>-</button>
-//   </div>
-// );
-// }
-// export default App;
